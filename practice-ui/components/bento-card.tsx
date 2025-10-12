95e63cbf746b74eff52d593d26e2fbee739d82c0
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-10-12 05:33:45 +0530

    bento-card

--- practice-ui/components/bento-card.tsx
+++ practice-ui/components/bento-card.tsx
@@ -8,7 +8,7 @@ export default function BentoCard() {
       <Header />
       <div className=" mask mask-b-from-60% grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 gap-2 my-8 md:border p-2 rounded-3xl md:border-neutral-900">
         <Colum>
-          <Card src="/images/1.jpeg" alt="first" href="https://uzwal.me"  className="rounded-tl-xl"  />
+          <Card src="/images/1.jpeg" alt="first" href="https://uzwal.me"  className="lg:rounded-tl-[calc(24px-8px)]"  />
           <Card src="/images/2.jpeg" alt="second"  href="#" className=""   />
           <Card src="/images/3.jpg" alt="third"  href="#"  className=""  />
           <Card src="/images/7.png" alt="Seven" href="#"  className=""   />
@@ -29,7 +29,7 @@ export default function BentoCard() {
           <Card src="/images/7.png" alt="Seven" href="#"  className=""  />
         </Colum>
         <Colum>
-          <Card src="/images/1.jpeg" alt="first" href="https://uzwal.me" className=""  />
+          <Card src="/images/1.jpeg" alt="first" href="https://uzwal.me" className="lg:rounded-tr-[calc(24px-8px)]"  />
           <Card src="/images/10.png" alt="Ten" href="https://linear.app" className=""   />
           <Card src="/images/11.png" alt="11" href="https://ui.shadcn.com" className=""   />
           <Card src="/images/7.png" alt="Seven"  href="#" className=""   />
@@ -60,10 +60,10 @@ const Colum = ({ children }: { children: React.ReactNode }) => {
 
 const Card = ({ src, alt, href ,className}: { src: string; alt: string,href:string ,className:string }) => {
   return (
-    <Link href={href} target="_blank" className=" group
-      after:content-['] after:absolute after:inset-0 after:bg-black/60 after:opacity-0 after:w-full after:h-full
+    <Link href={href} target="_blank" className={` group
+      after:content-[''] after:absolute after:inset-0 after:bg-black/60 after:opacity-0 after:w-full after:h-full
       hover:after:opacity-90 after:transition after:duration-300 max-w-xl mx-auto rounded-md
-      relative mb-2 overflow-hidden shadow-sm block group transition duration-300 ">
+      relative mb-2 overflow-hidden shadow-sm block group transition duration-300 ${className}`} > // for adding  extract style 
       <Image src={src} width={500} height={500} alt={alt} className="w-full" />
       <p className=" opacity-0   hover:opacity-100   z-20 transition-all duration-300
       absolute inset-0 text-white font-medium  items-center  m-auto justify-center  flex text-sm">{href.split("https://")[1]}</p>
