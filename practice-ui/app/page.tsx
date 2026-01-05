248fff4451dfa742c6f5db9bf4a1973b1ee04a35
isuzwal <ujjwalgaihre0@gmail.com>
2026-01-05 23:39:03 +0530

    make first button

--- practice-ui/app/page.tsx
+++ practice-ui/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import BentoCard from "@/components/bento-card";
+import BentoCard from "@/components/ui/bento-card";
+import Button from "@/components/ui/button";
+
 import { Screen } from "@/container/section-view";
 
 export default function Home() {
   return (
     <Screen>
-      <BentoCard />
+      {/* <BentoCard /> */}
+      <Button variant={"deafault"} size={"md"} className="bg-red-400">
+        Login
+      </Button>
     </Screen>
   );
 }
